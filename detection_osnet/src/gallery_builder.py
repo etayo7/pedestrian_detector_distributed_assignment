51c61fc9a2fcd7245a0887a418d86c1ffd569dd2
Mircea-Alexandru Popovici <mircea-alexandru.popovici@student.tuiasi.ro>
2022-05-29 23:42:04 +0200

    Gallery builder update - Gallery builder now supports nicknames for each person - Gallery builder saves each processed window as an image - Gallery builder saves galleries as PyTorch tensor files now

--- detection_osnet/src/gallery_builder.py
+++ detection_osnet/src/gallery_builder.py
@@ -3,7 +3,9 @@
 #   Imports
 
 import os
+from typing import List
 import cv2
+import torch
 from cv_bridge import CvBridge
 import rospy
 import rosbag
@@ -12,19 +14,17 @@ from reid import ReID
 import scipy
 import numpy
 from tkinter import filedialog
+from detection_osnet.msg import Window
+import imutils
 
 
 #   Defines
 
-os.environ['ROS_NAMESPACE'] = 'r_1'
+# os.environ['ROS_NAMESPACE'] = 'r_1'
 
 #   Globals
 
 #   Classes
-
-class MatObj:
-    def __init__(self):
-        pass
 
 #   Functions
 
@@ -61,24 +61,92 @@ def gallery_builder():
             )
         reid_obj = ReID(files, params)
 
+
     except Exception:
         rospy.logerr("Configuration info missing! Load configuration file / Run configuration script, then try again!")
         rospy.signal_shutdown("Node cannot run without configuration info!")
         return
-
+    
+    names = []
+    for i in range(target_no):
+        name = input(f'Please enter nickname for gallery {i}: ')
+        names.append(name)
     filetypes = (('JPEG Image', '*.jpg'), ('PNG Image', '*.png'))
     rospy.loginfo("Load images to create gallery...")
 
-    filenames = filedialog.askopenfilename(
+    filenames = filedialog.askopenfilenames(
         title='Select images...',
-        initialdir='.',
+        initialdir='~',
         filetypes=filetypes)
 
+    galleries : List['ReID.ReIDGallery'] = []
+    for i in range(target_no):
+        galleries.append(ReID.ReIDGallery(list()))
+    
+    try:
+        os.mkdir('results')
+        os.mkdir('results/thumbs')
+        os.mkdir('results/galleries')
+    except Exception:
+        pass
+    
     for fn in filenames:
         img = cv2.imread(fn)
+        height, width, channels = img.shape
+
+        # YOLO Detection
+        blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+        yolo_obj.network.net.setInput(blob)
+        outs = yolo_obj.network.net.forward(yolo_obj.network.output_layers)
+
+        boxes = []
+        for out in outs:
+            for detection in out:
+                if (detection[4] < yolo_obj.params.min_box_confidence):
+                    continue
+                scores = detection[5:]
+                class_id = numpy.argmax(scores)
+                score = scores[class_id]
+                if (score > yolo_obj.params.min_score) and (class_id == 0):
+                    # Object detected
+                    x = int(detection[0] * width)
+                    y = int(detection[1] * height)
+                    w = int(detection[2] * width)
+                    h = int(detection[3] * height)
+                    # Register data
+                    boxes.append(Window(x = x, y = y, w = w, h = h))
+        # Filter data
+        windows = []
+        for i in yolo_obj.filter_windows(boxes):
+            windows.append(boxes[i])
+
+        if len(windows) != target_no:
+            rospy.logwarn(f'Image ({fn}) did not return enough recognized persons and was skipped!')
+            continue
+        windows.sort(key=sort_key)
+
+        for i in range(target_no):
+            xLeft = int(max(0, windows[i].x - windows[i].w/2))
+            yUp = int(max(0, windows[i].y - windows[i].h/2))
+            xRight = int(min(width, windows[i].x + windows[i].w/2 - 1))
+            yDown = int(min(height, windows[i].y + windows[i].h/2 - 1))
+            cropped = img[yUp:yDown, xLeft:xRight]
+            # cropped = imutils.resize(cropped, width = reid_obj.params.window_w, height = int(reid_obj.params.window_w*reid_obj.params.window_ratio))
+            galleries[i].descriptors.append(reid_obj.extractor(cropped))
+            cv2.imwrite(f'results/thumbs/{names[i]}_{len(galleries[i].descriptors)}.jpg',cropped)
+    
+    for i in range(len(galleries)):
+        for j in range(len(galleries[i].descriptors)):
+            torch.save(galleries[i].descriptors[j], f'results/galleries/{names[i]}_d{j}.pt')
+
         
+
+
+
     pass
 
+def sort_key(w : Window):
+    return w.x
 #   Guard
 
 if __name__ == '__main__':    
